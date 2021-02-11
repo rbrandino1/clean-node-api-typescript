c32107050c9e0b4a71318e8046253ea142ea1177
Rafael R Brandino <rafael.brandino@softplan.com.br>
2021-02-11 18:05:37 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.export = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  testEnviroment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
